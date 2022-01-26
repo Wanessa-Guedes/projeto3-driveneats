a6f9368a05d82de0ecd5f6b0c5927298482f64fc
Wanessa-Guedes <wanessa.o.guedes@gmail.com>
2022-01-26 19:12:00 +0000

    Iniciando JS - Borda e check nas divs de comida

---
+++ script.js
@@ -0,0 +1,11 @@
+function escolherpedido(){
+    document.getElementById("escolha").style.border = "2px solid green";
+    document.getElementById("escolha").style.borderRadius = "5px";
+    document.getElementById("check1").style.display = "block";
+}
+
+
+
+function botaofecharpedido(){
+    document.getElementById("h3").style.backgroundColor = "blue";
+}
