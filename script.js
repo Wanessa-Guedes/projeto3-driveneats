39abeb38d039b215419decb95be6cb88790e09c0
Wanessa-Guedes <wanessa.o.guedes@gmail.com>
2022-01-28 12:11:15 +0000

    Tela de pedido final - sem whatsapp

--- script.js
+++ script.js
@@ -2,34 +2,46 @@
 
 var prato1 = document.getElementById("nomePrato1").innerHTML; 
 var preco1 = document.getElementById("precoPrato1").innerHTML;
+var precoFinal1 = parseFloat(preco1.replace(',', '.'));
 
 var prato2 = document.getElementById("nomePrato2").innerHTML; 
 var preco2 = document.getElementById("precoPrato2").innerHTML;
+var precoFinal2 = parseFloat(preco2.replace(',', '.'));
 
 var prato3 = document.getElementById("nomePrato3").innerHTML; 
 var preco3 = document.getElementById("precoPrato3").innerHTML;
+var precoFinal3 = parseFloat(preco3.replace(',', '.'));
+
+let soma = [];
+
 
 // VINCULANDO NOMES AS BEBIDAS
 
 var bebida1 = document.getElementById("nomeBebida1").innerHTML; 
 var preco4 = document.getElementById("precoBebida1").innerHTML;
+var precoFinal4 = parseFloat(preco4.replace(',', '.'));
 
 var bebida2 = document.getElementById("nomeBebida2").innerHTML; 
 var preco5 = document.getElementById("precoBebida2").innerHTML;
+var precoFinal5 = parseFloat(preco5.replace(',', '.'));
 
 var bebida3 = document.getElementById("nomeBebida3").innerHTML; 
 var preco6 = document.getElementById("precoBebida3").innerHTML;
+var precoFinal6 = parseFloat(preco6.replace(',', '.'));
 
 // VINCULANDO NOMES AS SOBREMESAS
 
 var sobremesa1 = document.getElementById("nomeSobremesa1").innerHTML; 
 var preco7 = document.getElementById("precoSobremesa1").innerHTML;
+var precoFinal7 = parseFloat(preco7.replace(',', '.'));
 
 var sobremesa2 = document.getElementById("nomeSobremesa2").innerHTML; 
 var preco8 = document.getElementById("precoSobremesa2").innerHTML;
+var precoFinal8 = parseFloat(preco8.replace(',', '.'));
 
 var sobremesa3 = document.getElementById("nomeSobremesa3").innerHTML; 
 var preco9 = document.getElementById("precoSobremesa3").innerHTML;
+var precoFinal9 = parseFloat(preco9.replace(',', '.'));
 
 // FUNÇÕES PARA SELECIONAR SOMENTE UMA COMIDA
 function escolherComida1(){
@@ -46,8 +58,10 @@ function escolherComida1(){
     document.getElementById("check3").style.display = "none";
 
     document.getElementById("pratoEscolhido").innerHTML = prato1;
-    document.getElementById("precoPratoEscolhido").innerHTML = preco1;
-
+    document.getElementById("precoPratoEscolhido").innerHTML = "R$ " +  preco1;
+    
+    soma[0] = precoFinal1;
+    
     checarPedidos();
 }
 
@@ -64,6 +78,11 @@ function escolherComida2(){
     document.getElementById("escolhaComida3").style.borderRadius = "9px";
     document.getElementById("check3").style.display = "none";
 
+    document.getElementById("pratoEscolhido").innerHTML = prato2;
+    document.getElementById("precoPratoEscolhido").innerHTML = "R$ " +  preco2;
+
+    soma[0] = precoFinal2;
+
     checarPedidos();
 }
 
@@ -79,6 +98,11 @@ function escolherComida3(){
     document.getElementById("escolhaComida2").style.border = "2px solid #FFFFFF";
     document.getElementById("escolhaComida2").style.borderRadius = "9px";
     document.getElementById("check2").style.display = "none";
+
+    document.getElementById("pratoEscolhido").innerHTML = prato3;
+    document.getElementById("precoPratoEscolhido").innerHTML = "R$ " + preco3;
+
+    soma[0] = precoFinal3;
 
     checarPedidos();
 }
@@ -97,6 +121,11 @@ function escolherBebida1(){
     document.getElementById("escolhaBebida3").style.borderRadius = "9px";
     document.getElementById("check6").style.display = "none";
 
+    document.getElementById("bebidaEscolhida").innerHTML = bebida1;
+    document.getElementById("precoBebidaEscolhida").innerHTML = "R$ " + preco4;
+
+    soma[1] = precoFinal4;
+    
     checarPedidos();
 }
 
@@ -113,6 +142,11 @@ function escolherBebida2(){
     document.getElementById("escolhaBebida3").style.borderRadius = "9px";
     document.getElementById("check6").style.display = "none";
 
+    document.getElementById("bebidaEscolhida").innerHTML = bebida2;
+    document.getElementById("precoBebidaEscolhida").innerHTML = "R$ " + preco5;
+
+    soma[1] = precoFinal5;
+
     checarPedidos();
 }
 
@@ -128,6 +162,11 @@ function escolherBebida3(){
     document.getElementById("escolhaBebida2").style.border = "2px solid #FFFFFF";
     document.getElementById("escolhaBebida2").style.borderRadius = "9px";
     document.getElementById("check5").style.display = "none";
+
+    document.getElementById("bebidaEscolhida").innerHTML = bebida3;
+    document.getElementById("precoBebidaEscolhida").innerHTML = "R$ " + preco6;
+
+    soma[1] = precoFinal6;
 
     checarPedidos();
 }
@@ -146,6 +185,11 @@ function escolherSobremesa1(){
     document.getElementById("escolhaSobremesa3").style.borderRadius = "9px";
     document.getElementById("check9").style.display = "none";
 
+    document.getElementById("sobremesaEscolhida").innerHTML = sobremesa1;
+    document.getElementById("precoSobremesaEscolhida").innerHTML = "R$ " + preco7;
+
+    soma[2] = precoFinal7;
+
     checarPedidos();
 }
 
@@ -162,6 +206,11 @@ function escolherSobremesa2(){
     document.getElementById("escolhaSobremesa3").style.borderRadius = "9px";
     document.getElementById("check9").style.display = "none";
 
+    document.getElementById("sobremesaEscolhida").innerHTML = sobremesa2;
+    document.getElementById("precoSobremesaEscolhida").innerHTML = "R$ " + preco8;
+
+    soma[2] = precoFinal8;
+
     checarPedidos();
 }
 
@@ -177,6 +226,11 @@ function escolherSobremesa3(){
     document.getElementById("escolhaSobremesa2").style.border = "2px solid #FFFFFF";
     document.getElementById("escolhaSobremesa2").style.borderRadius = "9px";
     document.getElementById("check8").style.display = "none";
+
+    document.getElementById("sobremesaEscolhida").innerHTML = sobremesa3;
+    document.getElementById("precoSobremesaEscolhida").innerHTML = "R$ " + preco9;
+
+    soma[2] = precoFinal9;
     
     checarPedidos();
 }
@@ -221,14 +275,31 @@ function botaofecharpedido(checkedOk){
         document.getElementById("bb").style.display= "flex";
         document.getElementById("bb").style.backgroundColor = "#32B72F";
         document.getElementById("h3").style.display= "none";
+        let sum = 0;
+        for (let i = 0; i < soma.length; i++) {
+            sum += soma[i];
+        }
+        let soma2 = sum.toFixed(2);
+        let stringSoma = "R$ " + soma2.replace('.', ',');
+        document.getElementById("totalPedido").innerHTML = stringSoma;
     }
 }
 
 function confirmaDados(){
-    document.getElementById("idHeader").style.opacity = "0.5";
+    document.getElementById("idHeader").style.opacity = "0.7";
     document.getElementById("idPratos").style.opacity = "0.5";
     document.getElementById("idBebidas").style.opacity = "0.5";
     document.getElementById("idSobremesas").style.opacity = "0.5";
-    document.getElementById("idBarraBottom").style.opacity = "0.5";
+    document.getElementById("idBarraBottom").style.opacity = "0.7";
     document.getElementById("blocoPedidoFinal").style.display = "flex";
+}
+
+function voltarPagina(){
+
+    document.getElementById("idHeader").style.opacity = "1";
+    document.getElementById("idPratos").style.opacity = "1";
+    document.getElementById("idBebidas").style.opacity = "1";
+    document.getElementById("idSobremesas").style.opacity = "1";
+    document.getElementById("idBarraBottom").style.opacity = "1";
+    document.getElementById("blocoPedidoFinal").style.display = "none";
 }
